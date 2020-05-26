96e835b3cbeb011ae2ba37b9b6047fa4df6abdbf
Jay-ArBrouillard <brouillardjr@gmail.com>
2020-05-25 19:37:01 -0500

    update to database creator

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 import sqlite3
 import json
-from datetime import datetime
+import time
 import zstandard as zstd
 import io
 import time
@@ -12,9 +12,16 @@ c = connection.cursor()
 print("Successfully connected to SQLite")
 
 def createTable():
-    c.execute("""CREATE TABLE IF NOT EXISTS parent_reply (comment_id TEXT PRIMARY KEY,
-                parent_id TEXT, parent_comment TEXT, reply_comment TEXT, subreddit TEXT,
-                unix INT, score INT)""")
+    c.execute("""CREATE TABLE IF NOT EXISTS parent_reply (parent_id TEXT PRIMARY KEY, comment_id TEXT UNIQUE, parent_comment TEXT, reply_comment TEXT, subreddit TEXT,
+                unix INT, parent_score INT, reply_score INT)""")
+
+def createIndex():
+    c.execute("""CREATE INDEX IF NOT EXISTS "index" ON "parent_reply" (
+	            "parent_id"
+            );""")
+
+def dropIndex():
+    c.execute("""DROP INDEX "index";""")
 
 def formatData(data):
     data = data.replace("\n", " ").replace("\r", " ").replace("\"", "'")
@@ -22,8 +29,7 @@ def formatData(data):
 
 def findParentComment(cid):
     try:
-        sql = "SELECT parent_comment, reply_comment, score FROM parent_reply WHERE comment_id = \"{}\" LIMIT 1".format(cid)
-        c.execute(sql)
+        c.execute("SELECT * FROM parent_reply WHERE comment_id = \"{}\"".format(cid))
         result = c.fetchone()
         if result != None:
             return result
@@ -33,8 +39,21 @@ def findParentComment(cid):
         print ("findParent", e)
         return False
 
+def findScore(cid):
+    try:
+        sql = "SELECT reply_score FROM parent_reply WHERE comment_id = \"{}\" LIMIT 1".format(cid)
+        c.execute(sql)
+        result = c.fetchone()
+        if result != None:
+            return result[0]
+        else:
+            return False
+    except Exception as e:
+        print ("findScore", e)
+        return False
+
 def isAcceptableBody(data):
-    if len(data.split(' ')) > 100 or len(data) < 1:
+    if len(data.split(' ')) > 50 or len(data) < 1:
         return False
     elif len(data) > 1000:
         return False
@@ -43,17 +62,32 @@ def isAcceptableBody(data):
     else:
         return True
 
-def addReplyComment(commentId, replyComment, score):
+def addReplyComment(parentId, replyComment, replyScore):
     try:
-        sql = "UPDATE parent_reply SET reply_comment = \"{}\", score = \"{}\" WHERE comment_id = \"{}\";".format(replyComment, score, commentId) 
-        c.execute(sql)
-        c.execute('COMMIT')
+        sql = "UPDATE parent_reply SET reply_comment = \"{}\", reply_score = {} WHERE parent_id = \"{}\";".format(replyComment, replyScore, parentId)
+        transaction_bldr(sql)
     except Exception as e:
         print ("addReplyComment", e)
     
-def insertComment(commentId, parentId, parentComment, subreddit, createdUTC, score):
-    sql = """INSERT INTO parent_reply (comment_id, parent_id, parent_comment, reply_comment, subreddit, unix, score) VALUES ("{}", "{}", "{}", NULL, "{}", {}, {});""".format(commentId, parentId, parentComment, subreddit, createdUTC, score) 
+def insertComment(commentId, parentId, parentComment, subreddit, createdUTC, parentScore, replyScore):
+    sql = """INSERT INTO parent_reply (comment_id, parent_id, parent_comment, reply_comment, subreddit, unix, parent_score, reply_score) VALUES ("{}", "{}", "{}", NULL, "{}", {}, {}, {});""".format(commentId, parentId, parentComment, subreddit, createdUTC, parentScore, replyScore) 
     transaction_bldr(sql)
+
+def deleteComment(parentId):
+    sql = "DELETE FROM parent_reply WHERE parent_id = \"{}\"".format(parentId)
+    transaction_bldr(sql)
+
+def findReply(parentId):
+    try:
+        c.execute("SELECT parent_comment, parent_score FROM parent_reply WHERE parent_id = \"{}\"".format(parentId))
+        result = c.fetchone()
+        if result != None:
+            return result
+        else:
+            return False
+    except Exception as e:
+        print ("findReply", e)
+        return False
 
 def transaction_bldr(sql):
     global sql_transactions
@@ -64,6 +98,8 @@ def transaction_bldr(sql):
 
 def executeBulkTransactions():
     global sql_transactions
+    c.execute('PRAGMA synchronous = OFF')
+    c.execute('PRAGMA cache_size = 100000')
     c.execute('BEGIN TRANSACTION')
     for query in sql_transactions:
         try:
@@ -77,6 +113,7 @@ if __name__ == "__main__":
     createTable()
     rowCounter = 0
     totalInserted = 0
+    start_time = time.time()
     with open('D:/chatbot-data/RC_2019-12.zst', 'rb') as fh:
         dctx = zstd.ZstdDecompressor()
         with dctx.stream_reader(fh) as reader:
@@ -85,8 +122,8 @@ if __name__ == "__main__":
             while line:
                 try:
                     row = json.loads(line)
-                    commentId = row['id']
                     parentId = row['parent_id'] #t3_ is a root comment, t1_ is a regular/reply comment
+                    commentId = row['id']
                     body = formatData(row['body'])
                     createdUTC = row['created_utc']
                     score = row['score']
@@ -94,42 +131,76 @@ if __name__ == "__main__":
                     rowCounter += 1
 
                     if score >= 2 and isAcceptableBody(body):
-                        insertComment(commentId, parentId, body, subreddit, createdUTC, score)
+                        insertComment(commentId, parentId, body, subreddit, createdUTC, score, 0)
                         totalInserted += 1
                     line = wrap.readline()
                 except:
                     line = wrap.readline()
                     pass
                 if rowCounter % 100000 == 0:
-                    print("Total rows read: {}, total inserted: {}, Time: {}".format(rowCounter, totalInserted, str(datetime.now())))
+                    print("Total rows read: {}, rows inserted: {}, Elapsed Time(hrs): {}".format(rowCounter, totalInserted, (time.time() - start_time) / 60 / 60))
+                
+    if sql_transactions:
+        executeBulkTransactions()
+    print("Finished Inserting! Total rows read: {}, total rows inserted: {}, Elapsed Time(hrs): {}".format(rowCounter, totalInserted, (time.time() - start_time) / 60 / 60))
 
-            if sql_transactions:
-                executeBulkTransactions()
+    print("Creating index on parent_id...")
+    createIndex()
+    print("Finish index on parent_id!")
 
-    print("Finished Inserting! Total rows read: {}, total inserted: {}, Time: {}".format(rowCounter, totalInserted, str(datetime.now())))
+    print("Removing rows that don't have a parent comment...")
+    rowCounter = 0
+    deletedRows = 0
+    for row in connection.execute('SELECT * FROM parent_reply'):
+        parentId = row[0]
+        if parentId.startswith("t1_"):
+            searchId = parentId[3:]
+            comment = findParentComment(searchId)
+            if not comment:
+                deleteComment(searchId)
+                deletedRows += 1
+        rowCounter += 1
+        if rowCounter % 100000 == 0:
+            print("Total rows read: {}, Deleted Rows: {}, Elapsed Time(hrs): {}".format(rowCounter, deletedRows, (time.time() - start_time) / 60 / 60))
+    if sql_transactions:
+        executeBulkTransactions()
+    print("Finished removing rows with empty replies! Total Rows Deleted {}, Elapsed Time(hrs): {}".format(deletedRows, (time.time() - start_time) / 60 / 60))
+
+    print("Updating rows with their reply...")
     rowCounter = 0
     pairedRows = 0
-    print("Updating rows with their replies...")
     for row in connection.execute('SELECT * FROM parent_reply'):
-        parentId = row[1]
-        commentId = row[0]
-        comment = row[2]
-        score = row[6]
-        if parentId.startswith("t1_"): #This comment is a reply to another comment
-            searchId = parentId[3:] #remove t1_
-            result = findParentComment(searchId)
-            if result:
-                if result[1] == None:
-                    addReplyComment(searchId, comment, score)
-                    pairedRows += 1
-                elif score > result[2]:
-                    addReplyComment(searchId, comment, score)
-        rowCounter += 1
-        if rowCounter % 10000 == 0:
-            print("Total rows read: {}, Paired Rows: {}, Time: {}".format(rowCounter, pairedRows, str(datetime.now())))
-    print("Finished Updating! Total rows read: {}, Total Paired Rows: {}, Time: {}".format(rowCounter, pairedRows, str(datetime.now())))
+        parentId = row[0]
+        commentId = row[1]
+        replyScore = row[7]
+        searchId = "t1_" + commentId
+        reply = findReply(searchId)
+        if reply and reply[1] > replyScore:
+            addReplyComment(parentId, reply[0], reply[1])
+            pairedRows += 1
 
-    print("Removing rows with empty replies...")
-    c.execute("DELETE FROM parent_reply WHERE reply_comment IS NULL")
-    c.execute('COMMIT')
-    print("Finished removing rows with empty replies!")
+        rowCounter += 1
+        if rowCounter % 100000 == 0:
+            print("Total rows read: {}, Paired Rows: {}, Elapsed Time(hrs): {}".format(rowCounter, pairedRows, (time.time() - start_time) / 60 / 60))
+    if sql_transactions:
+        executeBulkTransactions()
+    print("Finished updating rows with their reply! Total comment-reply pairs: {}, Elapsed Time(hrs): {}".format(pairedRows, (time.time() - start_time) / 60 / 60))
+
+    print("Removing rows with empty reply column...")
+    rowCounter = 0
+    removedRows = 0
+    for row in connection.execute('SELECT * FROM parent_reply'):
+        if not row[3]:
+            deleteComment(row[0])
+            removedRows += 1
+        rowCounter += 1
+        if rowCounter % 100000 == 0:
+            print("Total rows read: {}, Removed Rows (Empty): {}, Elapsed Time(hrs): {}".format(rowCounter, removedRows, (time.time() - start_time) / 60 / 60))
+    if sql_transactions:
+        executeBulkTransactions()
+
+    print("Finished! Total Removed Rows (Empty): {}, Total runtime (hrs): {} ".format(removedRows, (time.time() - start_time) / 60 / 60))
+
+    # print("Dropping index...")
+    # dropIndex()
+    # print("Finish removing index!")
